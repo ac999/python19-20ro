07c8586ac84b6747d47385c4bf63465766770704
Andrei C <andreicristian6@protonmail.com>
2019-12-25 22:45:24 +0200

    exercise2

---
+++ lottery.py
@@ -0,0 +1,22 @@
+'''
+Write a program called lottery.py which takes 3 arguments, all numbers and
+simulates a lottery extraction. 
+
+The first argument, max_nr, is the maximum number in the lottery. Min is 1,
+max is 49.
+
+The second argument, draw_size, represents how many numbers a player needs to
+choose. Min is 3, max is 10.
+
+The third argument is the number of players. Min is 2, max is 10.
+
+When called the program will first draw a winning combination of numbers
+( draw_size )
+
+and will randomly allocate draw_size numbers to each player.
+
+At the end lottery.py will output, on separate lines, the winning combination
+and for each
+
+player how many and the numbers they "guessed".
+'''
