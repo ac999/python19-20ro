f67ee72bb2565327f0102f3cf4890c6c47d2e5e7
Andrei C <andreicristian6@protonmail.com>
2019-12-29 15:54:57 +0200

    exercise 1, simple server/client

--- ex1/client.py
+++ ex1/client.py
@@ -3,7 +3,7 @@ import socket, sys
 if len(sys.argv) != 3:
     print("use: {} addr port".format(__file__))
     exit()
-
+# it will not check if the address is correct
 addr = sys.argv[1]
 try:
     port = int(sys.argv[2])
@@ -14,7 +14,7 @@ except Exception as e:
 s = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
 try:
     s.connect((addr,port))
-    # s.close()
+    s.close()
 except Exception as e:
     print(e)
     exit()
