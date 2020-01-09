821ffd7471ba75cdac728d906f0944054556f230
Andrei C <44598788+ac999@users.noreply.github.com>
2020-01-09 18:17:47 +0200

    Delete db.sql

--- db.sql
+++
@@ -1,8 +0,0 @@
-__pycache__ , /home/m0ric/ac999/python19-20ro , 2020-01-09 18:07:50
-script.py , /home/m0ric/ac999/python19-20ro , 2020-01-09 18:07:50
-r_script.py , /home/m0ric/ac999/python19-20ro , 2020-01-09 18:07:50
-test.zip , /home/m0ric/ac999/python19-20ro , 2020-01-09 18:07:50
-readme.MD , /home/m0ric/ac999/python19-20ro , 2020-01-09 18:07:50
-.git , /home/m0ric/ac999/python19-20ro , 2020-01-09 18:07:50
-db.sql , /home/m0ric/ac999/python19-20ro , 2020-01-09 18:07:50
-.gitignore , /home/m0ric/ac999/python19-20ro , 2020-01-09 18:07:50
